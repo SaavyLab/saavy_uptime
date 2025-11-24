64b72f812a1a802b5f1562bd3fc0409c16e88939
saavy eth <futurepetrucci@gmail.com>
2025-11-23 17:09:05 -0700

    rolled vite back to 6.x

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,7 +6,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // @ts-expect-error - tailwindcss plugin is not typed, this works but vite 7 is a bit wonky still
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
